0279a8a3523c05e25e0b90bbb36d4ad6bfb014ea
JamesBundgard <jamesbundgard@gmail.com>
2019-10-15 20:49:12 -0400

    Added redirect site.

--- Server/index.js
+++ Server/index.js
@@ -3,5 +3,7 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/test1', (req, res) => res.send('Hello World!'));
+app.get('/test2/plzwrk', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
