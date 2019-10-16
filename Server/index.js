c4c50b53cd81720cc9f130c0dc1a9d8c643d03ea
JamesBundgard <jamesbundgard@gmail.com>
2019-10-15 23:25:38 -0400

    Added GET request that returns the Pi's IP address.

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/test1', (req, res) => res.send('Hello World1!'));
-app.get('/test2/plzwrk', (req, res) => res.send('Hello World2!'));
+function getServerIP(res){
+    let pathToIP = path.join(__dirname, '..', 'ServerIP.txt');
+    fs.readFile(pathToIP, 'utf8', function(error, data) {
+        if (error) {
+            console.log('Error:- ' + error);
+            throw error;
+        }
+        else {
+            let result = "http://"+data.trim()+"/";;
+            console.log(result);
+            res.send(result);
+        }
+    })
+}
+
+app.get('/ping', (req, res) => res.send('Pong!'));
+app.get('/api/url', (req, res) => getServerIP(res));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
